dcb0b8815810488d1e4cfbae1c0971a87621d4d4
Ryan Sabo <sabora920@gmail.com>
2017-12-18 16:36:25 -0500

    added actions folder, reducers folder, store.js

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+export const MAKE_GUESS = 'MAKE_GUESS';
+
+export const makeGuess = guess => ({
+    type: MAKE_GUESS,
+    guess: 12
+})
+
+
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { MAKE_GUESS } from '../actions.js'
+
+const initialState = {
+    guesses: [],
+    feedback: 'Make your guess!',
+    auralStatus: '',
+    correctAnswer: Math.round(Math.random() * 100) + 1
+}
